de0c7c4f7adfd41817ffa2b485d6574aab8a485b
Pedro Maia <150867861+pedro-maia1337@users.noreply.github.com>
2024-11-18 15:38:31 -0300

    Update premio.py

--- premio.py
+++ premio.py
@@ -8,15 +8,15 @@ def verifica_premio(soma_total):
     return 'N'
 
 def main():
-    # Leitura das entradas
+    
     num_pao = int(input())
     num_doce = int(input())
     num_bolo = int(input())
 
-    # Cálculo da soma total
+    
     soma_total = num_pao * 1 + num_doce * 2 + num_bolo * 3
 
-    # Exibição do prêmio
+    
     print(verifica_premio(soma_total))
 
 if __name__ == "__main__":
